f7a37cc5cf00b0d9c12db7bcc10e7c84d3b2aa98
Kansetsu Kuo <kansetsu4@gmail.com>
2017-12-12 22:02:26 +0800

    change buttons style

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211030011) do
+ActiveRecord::Schema.define(version: 20171212094331) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20171211030011) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
-    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
