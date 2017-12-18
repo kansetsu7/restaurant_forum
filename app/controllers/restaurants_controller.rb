d83c9af29ae56a1e84d996137bfdc1b1dc479562
Kansetsu Kuo <kansetsu4@gmail.com>
2017-12-18 16:00:26 +0800

    add show page for public users

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,15 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
   end
+
+  def show
+    set_restaurant
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+  
 end
