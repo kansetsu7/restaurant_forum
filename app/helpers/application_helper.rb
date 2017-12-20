f899193fc837b457a23dd7322d916696b7b9dde3
Kansetsu Kuo <kansetsu4@gmail.com>
2017-12-20 23:14:02 +0800

    Bug fix: using clearfix to fix floating problems

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,4 +27,13 @@ module ApplicationHelper
     end
     link_to("Sort<span class='glyphicon glyphicon-sort-by-alphabet#{icon_direction}'></span>".html_safe, root_path(:direction => direction), class: "btn btn-default")
   end
+
+  def clearfixer(i)
+    return '<div class="clearfix hidden-xs"></div>'.html_safe if i == 5
+    if i.odd?
+      '<div class="clearfix hidden-xs hidden-md"></div>'.html_safe
+    elsif (i+1)%3 == 0
+      '<div class="clearfix hidden-xs hidden-sm"></div>'.html_safe
+    end
+  end
 end
