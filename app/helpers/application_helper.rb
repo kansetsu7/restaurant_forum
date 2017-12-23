793eaac05b7964a38f0af4cdb3e3deca4e3e3672
Kansetsu Kuo <kansetsu4@gmail.com>
2017-12-23 08:56:16 +0800

    Test for heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,12 +46,10 @@ module ApplicationHelper
     end
   end
 
-  def show_img(img, exist)
-    if exist
-      for img_id in 0..20
-        include_path = '/'+img_id.to_s+'.jpg'
-        return image_tag(('/seed_img'+include_path), class: "img-responsive center-block") if image_path(img).include?(include_path)
-      end
+  def show_img(img)
+    for img_id in 0..20
+      include_path = '/'+img_id.to_s+'.jpg'
+      return image_tag(('/seed_img'+include_path), class: "img-responsive center-block") if image_path(img).include?(include_path)
     end
     '<span class="glyphicon glyphicon-picture"></span>[沒有圖片]'.html_safe
   end
