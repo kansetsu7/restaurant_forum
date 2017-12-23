399768b632e1e53f9c9b85edbdf65f924725cea3
Kansetsu Kuo <kansetsu4@gmail.com>
2017-12-23 08:52:21 +0800

    Using helper to show img

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -45,4 +45,15 @@ module ApplicationHelper
       '<div class="clearfix hidden-xs hidden-sm hidden-lg"></div>'.html_safe
     end
   end
+
+  def show_img(img, exist)
+    if exist
+      for img_id in 0..20
+        include_path = '/'+img_id.to_s+'.jpg'
+        return image_tag(('/seed_img'+include_path), class: "img-responsive center-block") if image_path(img).include?(include_path)
+      end
+    end
+    '<span class="glyphicon glyphicon-picture"></span>[沒有圖片]'.html_safe
+  end
+
 end
