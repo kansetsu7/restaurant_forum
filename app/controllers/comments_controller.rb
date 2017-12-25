7ecfdc2e5542f94e2f85e4013e1f7aa1d1b81b1e
Kansetsu Kuo <kansetsu4@gmail.com>
2017-12-25 15:58:00 +0800

    Admin can delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,23 @@
 class CommentsController < ApplicationController
-  before_action :set_restaurant ,only: [:create]
+  before_action :set_restaurant ,only: [:create, :destroy]
+  before_action :set_comment ,only: [:destroy]
 
   def create
     @comment = @restaurant.comments.build(comment_params)
     @comment.user = current_user
     @comment.save!
     redirect_to restaurant_path(@restaurant)
+  end
 
+  def destroy
+    if current_user.admin?
+      flash[:notice] = "Comment by \"#{@comment.user.email.split('@')[0]}\" was successfully deleted"
+      @comment.destroy
+      redirect_to restaurant_path(@restaurant)
+    else
+      flash[:alert] = 'Not allow!'
+      redirect_to :back
+    end
   end
 
   private
@@ -17,6 +28,10 @@ class CommentsController < ApplicationController
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 
 end
