e2bdada6d07b94a3a6014ee5f0eff9c91445339a
Kansetsu Kuo <kansetsu4@gmail.com>
2017-12-25 09:32:48 +0800

    create comment model and set up relation between restaurant, comment and user

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,6 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name
   mount_uploader :image, PhotoUploader
   belongs_to :category, optional: true
+  has_many :comments
   delegate :name, to: :category, prefix: true, allow_nil: true
 end
