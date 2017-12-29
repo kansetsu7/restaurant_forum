974718b11fd32bc16b351e3f8e56eec02f1cb454
Kansetsu Kuo <kansetsu4@gmail.com>
2017-12-29 14:43:18 +0800

    Modify: delete comments when restaurant is deleted.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name
   mount_uploader :image, PhotoUploader
   belongs_to :category, optional: true
-  has_many :comments
+  has_many :comments, dependent: :destroy
   delegate :name, to: :category, prefix: true, allow_nil: true
 end
