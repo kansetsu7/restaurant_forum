82efb0c571d7487b2cdad09deba4f37ca71b38d4
Kansetsu Kuo <kansetsu4@gmail.com>
2018-01-05 15:44:03 +0800

    Create followship model and set up self-relationships within users table through followships

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
   has_many :likes, dependent: :destroy
   has_many :liked_restaurants, through: :likes, source: :restaurant
+  has_many :followships
+  has_many :followings, through: :followships
   before_save :ini_name
 
   mount_uploader :avatar, AvatarUploader
