4abde9cc5c85ea764b2c9d1dd0ec32c7e345a3fb
Kansetsu Kuo <kansetsu4@gmail.com>
2018-01-09 11:29:35 +0800

    Done: user can friend other user by clicking on the btn on user profile page.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,29 @@
 class FriendshipsController < ApplicationController
+  before_action :friendship_params ,only: [:create]
+  before_action :set_friendship ,only: [:destroy]
+
+  def create
+    @friendship = current_user.friendships.build(friendship_params)
+    if @friendship.save
+      flash[:notice] = 'Successfully friended!'
+    else
+      flash[:alert] = @friendship.errors.full_messages.to_sentence
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def friendship_params
+    params.permit(:friend_id)
+  end
+
+  def set_friendship
+    @friendship = current_user.friendships.find_by(friend_id: params[:id])
+  end
+
 end
