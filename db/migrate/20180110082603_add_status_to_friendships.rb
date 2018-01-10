8ac42c3f5f0a611e5c458e0aaa05159775465775
Kansetsu Kuo <kansetsu4@gmail.com>
2018-01-10 17:21:00 +0800

    Revert "add friend ship status column and remove confirmed column"
    This reverts commit 054ff253f69cef2b178f72ae98db0a4caa9f0d91.

--- db/migrate/20180110082603_add_status_to_friendships.rb
+++
@@ -1,6 +0,0 @@
-class AddStatusToFriendships < ActiveRecord::Migration[5.1]
-  def change
-    add_column :friendships, :status, :string, :default => 'unconfirmed', :null => false
-    remove_column :friendships, :confirmed
-  end
-end
