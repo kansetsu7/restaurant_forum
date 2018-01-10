04e81843bc4f02de6e2d1d69f162146520f0cce6
Kansetsu Kuo <kansetsu4@gmail.com>
2018-01-10 10:51:35 +0800

    Done: add btn for collapse on user profile

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -15,7 +15,7 @@ class FollowshipsController < ApplicationController
   def destroy
     @followship.destroy
     flash[:alert] = 'Unfollowed!'
-    redirect_to users_path
+    redirect_back(fallback_location: root_path)
   end
 
   private
